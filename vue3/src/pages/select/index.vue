62ccdc8df89dcfc95676ea81ac0b7854fa612824
xiaolin <462441355@qq.com>
2021-08-31 21:31:26 +0800

    * 添加vue3 测试文件

---
+++ vue3/src/pages/select/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        选车
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Select'
+}
+</script>
--- vue3/src/router/index.js
+++ vue3/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../pages/Home.vue'
 
 const routes = [
   {
@@ -13,7 +13,17 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue')
+  },
+  {
+    path: '/index',
+    name: 'Index',
+    component: () => import(/* webpackChunkName: "about" */ '../pages/index/index.vue')
+  },
+  {
+    path: '/select',
+    name: 'Select',
+    component: () => import(/* webpackChunkName: "about" */ '../pages/select/index.vue')
   }
 ]
 
