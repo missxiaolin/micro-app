bac946e1ff50272ed1b1d14d61867df292619dee
xiaolin <462441355@qq.com>
2021-09-17 08:26:45 +0800

    * vue2 改造

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -5,8 +5,32 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let instance = null
+
+const render = () => {
+  instance = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+}
+
+if (!window.__MICRO_WEB__) {
+  render()
+}
+
+// 开始加载结构
+export const bootstrap = () => {
+  console.log('加载成功')
+}
+
+// 渲染成功
+export const mount = () => {
+  render()
+  console.log('渲染成功')
+}
+
+// 卸载成功
+export const unmount = () => {
+  console.log('卸载', instance)
+}
