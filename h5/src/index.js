421fd0ad03e07ec72de257e3ed36a54ea5ca0409
xiaolin <462441355@qq.com>
2021-09-18 21:22:54 +0800

    （* main.js

--- h5/src/index.js
+++ h5/src/index.js
@@ -4,14 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+
+
+
+let instance = null
+
+const render = () => {
+  instance = ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  reportWebVitals();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+if (!window.__MICRO_WEB__) {
+  render()
+}
+
+
+// 开始加载结构
+export const bootstrap = () => {
+  console.log('加载成功')
+}
+
+// 渲染成功
+export const mount = () => {
+  render()
+  console.log('渲染成功')
+}
+
+// 卸载成功
+export const unmount = () => {
+  console.log('卸载', instance)
+}
