02ac33f3e32ea986e2ddaa0fc28303fde7cda069
xiaolin <462441355@qq.com>
2021-09-18 20:46:14 +0800

    * vue3 改造微前端

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -3,4 +3,29 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+let instance = null
+
+const render = () => {
+    instance = createApp(App).use(store).use(router).mount('#app')
+}
+
+if (!window.__MICRO_WEB__) {
+    render()
+}
+
+
+// 开始加载结构
+export const bootstrap = () => {
+    console.log('加载成功')
+}
+
+// 渲染成功
+export const mount = () => {
+    render()
+    console.log('渲染成功')
+}
+
+// 卸载成功
+export const unmount = () => {
+    console.log('卸载', instance)
+}
