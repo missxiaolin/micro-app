760b1d5deed2f19fe999042652450f3f92797b19
xiaolin <462441355@qq.com>
2021-09-22 21:14:37 +0800

    * 路由拦截

--- master/micro/start.js
+++ master/micro/start.js
@@ -1,5 +1,8 @@
 import { setList } from './const/subApps'
+import { rewriteRouter } from './router/rewriteRouter'
 
+// 实现路由拦截
+rewriteRouter()
 
 export const registerMicroApps = (appList) => {
     setList(appList)
---
+++ master/micro/utils/index.js
@@ -0,0 +1,9 @@
+
+// 给当前的路由跳转打补丁
+export const patchRouter = (globalEvent, eventName) => {
+    return function () {
+        const e = new Event(eventName)
+        globalEvent.apply(this, arguments)
+        window.dispatchEvent(e)
+    }
+}
