ac9c26bec77094cf8246731fc8d901035416dfac
xiaolin <462441355@qq.com>
2021-09-23 10:08:58 +0800

    * 删除注释

--- master/micro/router/rewriteRouter.js
+++ master/micro/router/rewriteRouter.js
@@ -7,7 +7,8 @@ export const rewriteRouter = () => {
     window.history.replaceState = patchRouter(window.history.replaceState, 'micro_replace')
     window.addEventListener('micro_push', turnApp)
     window.addEventListener('micro_replace', turnApp)
-
+    
+    // 监听返回事件
     window.onpopstate = function() {
         turnApp()
     }
