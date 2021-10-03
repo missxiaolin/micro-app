21117d77023a42555e43d5b1a49e4ef593775deb
xiaolin <462441355@qq.com>
2021-10-03 16:41:17 +0800

    * 应用修改

--- master/src/const/nav.js
+++ master/src/const/nav.js
@@ -1,23 +1,16 @@
 export const NAV_LIST = [
     {
       name: '首页',
-      status: true,
+      status: false,
       value: 0,
       url: '/vue3/#/index',
       hash: '',
     },
-    // {
-    //   name: '测试1',
-    //   status: false,
-    //   value: 3,
-    //   url: '/vue3/#/select',
-    //   hash: '',
-    // },
     {
       name: '测试2',
       status: false,
-      value: 4,
-      url: '/vue2#/energy',
+      value: 1,
+      url: '/vue2/#/energy',
       hash: '',
     },
   ]
