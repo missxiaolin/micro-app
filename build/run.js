330db13e689e95f21337fad455b373b11ef066a0
xiaolin <462441355@qq.com>
2021-10-18 15:29:02 +0800

    * 脚本修改，一次性启动所有应用

--- build/run.js
+++ build/run.js
@@ -2,10 +2,11 @@ const childProcess = require('child_process')
 const path = require('path')
 
 const filePath = {
-    vue2: path.join(__dirname, '../vue2'),
-    vue3: path.join(__dirname, '../vue3'),
-    react: path.join(__dirname, '../h5'),
-    master: path.join(__dirname, '../master')
+    sad: path.join(__dirname, '../react15'),
+    acc: path.join(__dirname, '../react16'),
+    vac: path.join(__dirname, '../vue2'),
+    mac: path.join(__dirname, '../vue2_c'),
+    mad: path.join(__dirname, '../vue3')
 }
 
 // cd 子应用的目录 npm start 启动项目
